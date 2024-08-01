e9122fad75702156ad3a285e7c275356b852f402
Attu Raphael <raphaelattu558@gmail.com>
2024-08-01 12:20:44 +0100

    save the changes

--- Framework/Screens/Home.js
+++ Framework/Screens/Home.js
@@ -9,15 +9,25 @@ export function Home(){
                 <View> Logo</View>
                 <View>menu</View>
             </View>
-            <View>
+            <View style={styles.intro}>
                 <View>
-                    <View>
-                        <Image />
+                    <View style={styles.imageContainer}>
+                        <Image style={{width:49,height:49,borderEndStartRadius:24.5}} />
                     </View>
-                    <View>
-                        <Text>Hi {userinformstion.firstName}</Text>
+                    <View style={styles.balanceField}>
+                        <Text>Hi Attu</Text>
+                        <Text>Balance:0</Text>
                     </View>
                 </View>
+                <View
+                style={styles.cauroselField}>
+
+                </View>
+                <View style={styles.logOutField}>
+
+                </View>
+            </View>
+            <View>
                 <View></View>
                 <View></View>
             </View>
@@ -43,12 +53,33 @@ const styles=StyleSheet.create({
     container:{
         marginTop:StatusBar.currentHeight
     },
+    imageContainer:{
+        width:50,
+        height:50,
+        borderRadius:20,
+        borderWidth:4,
+        borderColor:"yellow"
+    },
     header:{
 width:40,
 borderEndStartRadius:5,
 borderWidth:2,
-borderColor:"yellow",
+borderColor:"#yellow",
 flex:1,
 flexDirection:"column"
+    },
+    intro:{
+        height:70,
+        width:70,
+    },
+   balanceField:{
+        width:30,
+        height:30,
+    },
+    cauroselField:{
+        flex:1
+    },
+    logOutField:{
+        width:30
     }
 })
